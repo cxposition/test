142423d6af7394e35210a888c4d54880341657c1
cxposition <2213087097@qq.com>
2026-08-12 16:30:08 +0800

    fix: 修正健康检查接口路径

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 		_, _ = w.Write([]byte("ok"))
 	})
 
-	http.HandleFunc("/healthzzzz", func(w http.ResponseWriter, _ *http.Request) {
+	http.HandleFunc("/healthzzzzzz", func(w http.ResponseWriter, _ *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write([]byte("ok"))
 	})
